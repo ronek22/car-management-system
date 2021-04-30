98ad02894ed6a67b8f6e167bf1b90a58c7794e68
ronek22 <jronek3010@gmail.com>
2021-04-30 12:38:13 +0200

    Edit offer from table view

--- frontend/src/app/offer/offer.component.ts
+++ frontend/src/app/offer/offer.component.ts
@@ -5,25 +5,26 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { Offer, Pagination, PaginationOffer } from '../models/models';
-import { OfferService } from '../services/offer.service';
+import {Offer, Pagination, PaginationOffer} from '../models/models';
+import {OfferService} from '../services/offer.service';
 import {
   ColumnMode,
   DatatableComponent,
   SelectionType,
 } from '@swimlane/ngx-datatable';
-import { OfferDialogComponent } from './dialogs/offer-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import { map, switchMap } from 'rxjs/operators';
-import { empty } from 'rxjs';
-import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import {OfferDialogComponent} from './dialogs/offer-dialog.component';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {map, switchMap} from 'rxjs/operators';
+import {empty} from 'rxjs';
+import {ConfirmationDialogComponent} from '../confirmation-dialog/confirmation-dialog.component';
+import {EditOfferComponent} from "./offer-detail/edit-offer/edit-offer.component";
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.scss'],
 })
-export class OfferComponent implements OnInit, AfterViewInit {
+export class OfferComponent implements OnInit {
   isLoading = true;
 
   @ViewChild(DatatableComponent) ngxDatatable: DatatableComponent;
@@ -33,14 +34,14 @@ export class OfferComponent implements OnInit, AfterViewInit {
     // {name: 'ID', prop: 'id', sortable: false},
     {prop: 'car.year', name: 'Rocznik', width: 100, sortable: true},
     {prop: 'car.make.name', name: 'Marka', width: 100, sortable: true},
-    {prop: 'car.model', name: 'Model', width:150, sortable: true},
+    {prop: 'car.model', name: 'Model', width: 150, sortable: true},
     {prop: 'vin', name: 'VIN', width: 200, sortable: true},
-    {prop: 'client', name: 'Klient', width:200, sortable: true},
-    {prop: 'customer.email', name: 'Email', width:250, sortable: true},
+    {prop: 'client', name: 'Klient', width: 200, sortable: true},
+    {prop: 'customer.email', name: 'Email', width: 250, sortable: true},
     {prop: 'pay_for_transport', name: 'Zapłata za transport', width: 150, sortable: false},
     {prop: 'ship_documents_to_agency', name: 'Wysyłka dokumentów', width: 150, sortable: false},
-    {prop: 'broker.name', name: 'Broker', width:150, sortable: false},
-    {prop: 'over_fracht', name: 'Over fracht', width: 150,  sortable: false},
+    {prop: 'broker.name', name: 'Broker', width: 150, sortable: false},
+    {prop: 'over_fracht', name: 'Over fracht', width: 150, sortable: false},
     {prop: 'over_odprawa', name: 'Over odprawa', width: 150, sortable: false},
     {prop: 'over_transport_to_pl', name: 'Over transport to PL', width: 150, sortable: false},
     {prop: 'employeeFullname', name: 'Kupił', width: 200, sortable: false},
@@ -51,7 +52,8 @@ export class OfferComponent implements OnInit, AfterViewInit {
   selected = [];
   SelectionType = SelectionType;
 
-  constructor(private offerService: OfferService, public dialog: MatDialog) {}
+  constructor(private offerService: OfferService, public dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     this.offerService.getOfferList().subscribe((response) => {
@@ -71,21 +73,10 @@ export class OfferComponent implements OnInit, AfterViewInit {
     }));
   }
 
-  ngAfterViewInit(): void {
-    // this.ngxDatatable.columnMode = ColumnMode.force;
-  }
 
   singleSelectCheck = (row: any) => {
     return this.selected.indexOf(row) === -1;
   };
-
-  onSelect({ selected }): void {
-    console.log('Select event', this.selected);
-  }
-
-  onActivate(event): void {
-    console.log('Activate event', event);
-  }
 
   setPage(pageInfo): void {
     console.log(pageInfo.offset);
@@ -165,6 +156,41 @@ export class OfferComponent implements OnInit, AfterViewInit {
       .subscribe((response) => {
         this.subscribeOffers(response);
 
+        this.selected = [];
+      });
+  }
+
+  editOffer(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = this.selected[0];
+    dialogConfig.disableClose = true;
+
+    const dialogComponent = EditOfferComponent;
+    const dialogRef = this.dialog.open(dialogComponent, dialogConfig);
+
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        switchMap((result) => {
+          if (!result) {
+            return empty();
+          }
+          return this.offerService.editOffer(result);
+        })
+      )
+      .pipe(
+        switchMap(() => {
+          this.isLoading = true;
+          return this.offerService.getOfferList();
+        }),
+        map((data) => {
+          this.isLoading = false;
+          return data;
+        })
+      )
+      .subscribe((response) => {
+        this.subscribeOffers(response);
         this.selected = [];
       });
   }
